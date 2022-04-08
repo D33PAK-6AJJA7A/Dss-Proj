5d06fdd09dfcf31af98827094434792917ba5ef7
Jyotika999 <Jyotika9september@gmail.com>
2022-04-08 11:58:58 +0530

    Added random text file generator

---
+++ text_file_generator.py
@@ -0,0 +1,19 @@
+
+import random
+
+
+def text_file_generator(users, pathfile):
+  f = open(pathfile, "w")
+  f.write(str(users)+"\n")
+  for i in range(1,51):
+    sender = random.randint(0, users-1)
+    receiver = random.randint(0, users-1)
+    if(sender!=receiver):
+      f.write( "User" + str(sender)+" | "+  "User"+ str(receiver) + " | "+ "message" + str(i) + "\n");
+    else:
+      f.write( "User" + str(sender) + " | " + "User" + str((receiver+1)%users) + " | " + "message" + str(i)+"\n");
+  f.close()
+
+
+text_file_generator(3,"text.txt" )
+
