d1365eb4e97d190d46fc9cfb13af0d59e18b91d5
Krishna Kaipa <kaipakrishna380@gmail.com>
2022-04-10 01:55:41 +0530

    1. User can end convo using "__end__", ends conversation for other user as well. 2. Added function to write all logs to file. 3. Moved non-functional code to Old file

--- Old/client.py
+++ Old/client.py
@@ -1,6 +1,7 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 from random import randint
+import os
 
 class Client(DatagramProtocol):
      def __init__ (self, host, port):
@@ -10,6 +11,10 @@ class Client(DatagramProtocol):
           self.address = None
           self.id = host, port
           self.server = "127.0.0.1", 9999
+          print("Instructions : ")
+          print("__end__ : Stops communication and closes the client")
+          print("__users__ : To get a list of current online users")
+          print("__connect__ : To connect to other online user")
           print("Working on id:", self.id)
 
      def startProtocol(self):
@@ -19,6 +24,7 @@ class Client(DatagramProtocol):
      #      self.transport.write("end".encode("utf -8"), self.server)          
      
      def datagramReceived(self, datagram, addr):
+          # print("hi")
           datagram = datagram.decode("utf-8")      
      
           if addr == self.server:
@@ -26,15 +32,32 @@ class Client(DatagramProtocol):
                self.address = "127.0.0.1" , int(input("Write port: "))
                reactor.callInThread(self.send_message)
           else:
+               if datagram == "__end__":
+                    print("User has ended conversation")
+                    reactor.stop()
+                    os._exit(0)
                print(addr, ":", datagram)
+               print("\n-->",end="")
    
      def send_message(self):
+          print("-->",end="")
           while True:
-               ip = input("::: ") 
+               ip = input("") 
                if(ip == "__end__"):
                     self.transport.write("end".encode('utf-8'), self.server)
+                    self.transport.write("__end__".encode('utf-8'), self.address)
+                    reactor.stop()
+                    os._exit(0)
+
+               elif(ip == "__users__"):
+                    self.transport.write("users".encode('utf-8'), self.server)
                else:
                     self.transport.write(ip.encode('utf-8'), self.address)
+                    file = open("text_1_1.txt", "a")
+                    #from user1837 to user28372 :::: message
+                    line = "from user"+str(self.id[1]) + " to user" + str(self.address[1]) + " :::: "+str(ip)+"\n"
+                    file.write(line)
+                    file.close()
 
 if __name__ == '__main__' :
      port = randint(1025, 7000)
--- server.py
+++ server.py
@@ -4,7 +4,10 @@ from twisted.internet import reactor
 class Server(DatagramProtocol):
     def __init__(self):
         self.clients = set()
-    
+        self.file = open("text_1_1.txt", "w+")
+        self.file.write("5\n")
+        self.file.close()
+
     def datagramReceived (self, datagram, addr):
         datagram = datagram.decode( "utf-8")
 
@@ -13,6 +16,10 @@ class Server(DatagramProtocol):
             print("\nNew client joined",addr[1])
             print("Current list of clients: ",", ".join(str(x) for _,x in self.clients))
             self.transport.write("\n".join(str(x) for x in self.clients).encode(), addr)
+        
+        elif datagram == "users":
+            self.transport.write("\n".join(str(x) for _,x in self.clients).encode(), addr)        
+        
         
         elif datagram == "end":   
             print("\nClient left",addr[1])
