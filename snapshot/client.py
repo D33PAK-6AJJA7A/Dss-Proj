aad5d2d712b3768182fceda3f38207223ceb3568
GAJJALA DEEPAK <iib2019024@iiita.ac.in>
2022-04-16 14:10:46 +0530

    implemented snapshot algo

---
+++ snapshot/client.py
@@ -0,0 +1,158 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+from random import randint
+import os
+import time
+import pickle
+
+
+connect_flag = 0
+user_flag = 0
+recv_flag = 0
+
+class Client(DatagramProtocol):
+     def __init__ (self, host, port):
+          if host == "localhost":
+               host = "127.0.0.1" 
+
+          self.address = None
+          self.id = host, port
+          self.server = "127.0.0.1", 9999
+          self.name = ""
+          self.other_user = ""
+          print("Menu commands : ")
+          print("__end__ : Stops communication and closes the client")
+          print("__users__ : To get a list of current online users")
+          print("__connect__ : To connect to other online user")
+          print("__chats__ : To get all previous chats")
+          print("__simulate__ : To sumulate random conversations")
+          print("__snapshot__ : Retrieves latest global snapshot")
+          print("Working on id:", self.id)
+
+     def startProtocol(self):
+          name = input("Name: ")
+          self.name = name
+          line = "ready:"+name
+          self.transport.write(line.encode('utf -8'), self.server)
+          reactor.callInThread(self.poll_connect)
+     
+
+     def poll_connect(self):
+
+          global connect_flag
+          global user_flag
+          global recv_flag
+
+          while True:
+               ip = input("Enter menu command: ")
+               if ip == "__users__":
+                    user_flag = 1
+                    self.transport.write("users".encode('utf -8'), self.server)
+                    time.sleep(1)
+               elif ip == "__simulate__" :
+                    users = int(input("Enter no. of users to chat : "))
+                    line = "simulate:"+str(users)
+                    self.transport.write(line.encode('utf -8'), self.server)
+
+               elif ip == "__snapshot__" :
+                   reactor.callInThread(self.file_polling)
+
+               elif ip == "__connect__":
+                    connect_flag = 1
+                    recv_flag = 1
+                    name = input("Enter name of user: ")
+                    self.other_user = name
+                    line = "query:"+name
+                    self.transport.write(line.encode('utf -8'), self.server)
+                    break
+          
+          self.transport.write("users".encode('utf -8'), self.server) 
+     
+
+     def datagramReceived(self, datagram, addr):
+
+          global connect_flag
+          global user_flag
+          global recv_flag
+          # print("hi")
+          datagram = datagram.decode('utf-8')      
+
+          if datagram.startswith("Simm:"):
+               # print(datagram)
+               lib = datagram.split(":")
+               from_name = lib[1]
+               to_port = int(lib[2])
+               ip = lib[3]
+               to_addr = "127.0.0.1", to_port
+               print("--> ", ip, " : ", to_port)
+               self.transport.write(("Simm_recv:" + from_name +":" + ip).encode('utf-8'), to_addr)
+
+          elif datagram.startswith("Simm_recv"):
+               tot = datagram.split(":")
+               msg = tot[2]
+               name = tot[1]
+               print(name, " : ", msg)
+
+          if connect_flag == 1:
+               connect_flag = 0
+               port = int(datagram)
+               self.address = "127.0.0.1" , port
+               reactor.callInThread(self.send_message)
+          else:
+               if datagram == "__end__":
+                    print(self.other_user+" has ended the conversation")
+                    reactor.stop()
+                    os._exit(0)
+               
+               if user_flag == 1:
+                    user_flag = 0
+                    print("Users: ",datagram)
+               elif recv_flag == 1 and datagram.startswith("|msg|") :
+                    tot = datagram.split(":")
+                    msg = tot[2]
+                    tim = tot[1]
+                    print(self.other_user, " : ", msg)
+                    line = {"Communication Status" : "successfully recieved", "Sent at" : tim, "Recieved at" : str(time.time()), "msg" : msg, "from" : self.name, "to" : self.other_user}
+                    f = open("snap_file.pkl","wb")
+                    pickle.dump(line,f)
+                    f.close()
+   
+     def send_message(self):
+          while True:
+               ip = input("--> ") 
+               if(ip == "__end__"):
+                    self.transport.write("end".encode('utf-8'), self.server)
+                    self.transport.write("__end__".encode('utf-8'), self.address)
+                    reactor.stop()
+                    os._exit(0)
+
+               elif(ip == "__users__"):
+                    self.transport.write("users".encode('utf-8'), self.server)
+                
+               elif ip == "__snapshot__" :
+                   reactor.callInThread(self.file_polling)
+
+               else:
+                    line = {"Communication Status" : "On the way", "Sent at" : str(time.time()), "Recieved at" : "-", "msg" : ip, "from" : self.name, "to" : self.other_user}
+                    f = open("snap_file.pkl","wb")
+                    pickle.dump(line,f)
+                    f.close()
+                    self.transport.write(("|msg|:"+str(time.time()) + ":" + ip).encode('utf-8'), self.address)
+
+     def file_polling(self):
+        last_dict = None
+        with (open("snap_file.pkl", "rb")) as openfile:
+            while True:
+                try:
+                    last_dict = pickle.load(openfile)
+                except EOFError:
+                    break
+        if last_dict != None :
+            print(last_dict + "\n")
+        else:
+            print("No Communication yet. \n")
+
+if __name__ == '__main__' :
+     port = randint(1025, 7000)
+     reactor.listenUDP(port, Client('localhost', port))
+     reactor.run()
---
+++ snapshot/server.py
@@ -0,0 +1,86 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor,task
+import random
+from threading import Thread
+import time
+import pickle
+
+class Server(DatagramProtocol):
+    def __init__(self):
+        self.clients = set()
+        self.f1 = open("text_1_2.txt", "w+")
+        self.f1.close()
+        self.names = {}
+
+    def datagramReceived (self, datagram, addr):
+        datagram = datagram.decode('utf-8')
+ 
+        if datagram.startswith("ready"):
+            lst = datagram.split(":")
+            self.names[lst[1]] = addr[1]
+            self.clients.add(addr)
+            print("\nNew client joined",addr[1])
+            print("Current list of clients: ",", ".join(str(x) for _,x in self.clients))
+            print(self.names)
+        
+        elif datagram == "users":
+            self.transport.write(", ".join(x for x in self.names.keys()).encode(), addr)        
+
+        elif datagram.startswith("query"):
+            lst = datagram.split(":")
+            name = lst[1]
+            port = self.names[name]
+            self.transport.write(str(port).encode(), addr)
+
+        elif datagram.startswith("simulate"):
+            lst = datagram.split(":")
+            users = int(lst[1])
+            f = open("text_1_2.txt", "a")
+            f.write(str(users)+"\n")
+            for i in range(1,51):
+                sender = random.randint(0, users-1)
+                receiver = random.randint(0, users-1)
+                if(sender!=receiver):
+                    f.write( "User" + str(sender)+" | "+  "User"+ str(receiver) + " | "+ "message" + str(i) + "\n")
+                    s_addr = "127.0.0.1", self.names["User" + str(sender)]
+                    to_port = self.names["User" + str(receiver)]
+                    self.transport.write(("Simm:"+"User" + str(sender) + ":" + str(to_port) + ":message" + str(i)).encode(), s_addr)
+                else:
+                    f.write( "User" + str(sender) + " | " + "User" + str((receiver+1)%users) + " | " + "message" + str(i)+"\n")
+                    s_addr = "127.0.0.1", self.names["User" + str(sender)]
+                    to_port = self.names["User" + str((receiver+1)%users)]
+                    self.transport.write(("Simm:" +"User" + str(sender) + ":" + str(to_port) + ":message" + str(i)).encode(), s_addr)
+            f.close()
+        
+        elif datagram == "end":   
+            print("\nClient left",addr[1])
+            self.clients.remove(addr)
+            self.names.pop(self.names.keys()[list(self.names.values()).index(addr[1])])
+            print("Current list of clients: ","\n".join(str(x) for _,x in self.clients))
+            print(self.names)
+
+
+def file_polling():
+    last_dict = None
+    with (open("snap_file.pkl", "rb")) as openfile:
+        while True:
+            try:
+                last_dict = pickle.load(openfile)
+            except EOFError:
+                break
+    if last_dict != None :
+        if last_dict["Recieved at"] != "-" and time.time() - float(last_dict["Recieved at"]) > 2 :
+            line = {"Communication Status" : "No communication", "Sent at" : "-", "Recieved at" : "-", "msg" : "-", "from" : "-", "to" : "-"}
+            f = open("snap_file.pkl","wb")
+            pickle.dump(line,f)
+            f.close()
+
+
+if __name__ ==  '__main__' :
+    reactor.listenUDP(9999, Server())
+    l = task.LoopingCall(file_polling)
+    l.start(2)
+    reactor.run()
+    # thread = Thread(target = file_polling, args = ())
+    # thread.start()
+    # thread.join()
